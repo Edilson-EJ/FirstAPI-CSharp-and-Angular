134ba91e2a4e0cfb37a8f42be986ae2587d0f285
Edilson-EJ <edilson.franca.melo@gmail.com>
2024-10-07 08:43:31 -0300

    adjusting comments and variables to English.

--- back-CSharp/Models/Pessoa.cs
+++ back-CSharp/Models/Pessoa.cs
@@ -2,12 +2,53 @@ namespace ApiAngularCsharp.Models;
 
 public class Pessoa
 {
-    public Guid? Id { get; set; }
-    public string Nome { get; set; }
+    private Guid id;
+    private int idade;
+    private string nome;
+    private string email;
+    private DateTime dataNascimento;
+    public Guid? Id
+    {
+        get => id;
+        set => id = (Guid)value;
+    }
+    
+    public int Idade 
+    { 
+        get => idade; 
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Age cannot be negative.");
+            idade = value;
+        }
+    }
 
-    public Pessoa(Guid id, string nome)
+    public string Nome
+    {
+        get => nome;
+        set => nome = value;
+    }
+
+    public string Email
+    {
+        get => email;
+        set => email = value;
+    }
+
+    public DateTime DataNascimento
+    {
+        get => dataNascimento;
+        set => dataNascimento = value;
+    }
+
+    public Pessoa(Guid id, string nome, string email, int idade, DateTime dataNascimento)
     {
         Id = id;
         Nome = nome;
+        Email = email;
+        Idade = idade;
+        DataNascimento = dataNascimento;
     }
 }
+
--- back-CSharp/Rotas/PessoaRotas.cs
+++ back-CSharp/Rotas/PessoaRotas.cs
@@ -7,9 +7,27 @@ public static class PessoaRotas
 {
     public static List<Pessoa> Pessoas = new()
     {
-        new Pessoa(id:Guid.NewGuid(), nome:"Neymar"),
-        new Pessoa(id:Guid.NewGuid(), nome:"Edilson"),
-        new Pessoa(id:Guid.NewGuid(), nome:"CR7"),
+        new Pessoa(
+            id: Guid.NewGuid(),
+            nome: "Neymar",
+            email: "neymar@example.com",
+            idade: 31,
+            dataNascimento: DateTime.Parse("05/02/1992") // Convertendo para DateTime
+        ),
+        new Pessoa(
+            id: Guid.NewGuid(),
+            nome: "Edilson",
+            email: "edilson@example.com",
+            idade: 28,
+            dataNascimento: DateTime.Parse("10/03/1996") // Convertendo para DateTime
+        ),
+        new Pessoa(
+            id: Guid.NewGuid(),
+            nome: "CR7",
+            email: "cr7@example.com",
+            idade: 39,
+            dataNascimento: DateTime.Parse("05/02/1985") // Convertendo para DateTime
+        )
     };
 
     public static void MapPessoaRotas(this WebApplication app)
@@ -19,7 +37,7 @@ public static class PessoaRotas
 
         // Rota para obter uma pessoa pelo nome
         app.MapGet("/pessoas/{nome}", 
-            handler: (string nome) => Pessoas.Find(p => p.Nome.StartsWith((nome))));
+            handler: (string nome) => Pessoas.Find(p => p.Nome.StartsWith(nome)));
 
         // Rota para adicionar uma nova pessoa
         app.MapPost("/pessoas/create", (HttpContext context, [FromBody] Pessoa pessoa) =>
@@ -29,11 +47,9 @@ public static class PessoaRotas
             return Results.Ok(pessoa);
         });
 
-
         // Rota para atualizar uma pessoa pelo ID
         app.MapPut(pattern: "/pessoa/{id}", handler: (Guid id, Pessoa pessoa) =>
         {
-            // Corrigindo a declaração da variável 'encontrado'
             var encontrado = Pessoas.Find(x => x.Id == id);
 
             if (encontrado == null)
@@ -41,6 +57,9 @@ public static class PessoaRotas
 
             // Atualiza as propriedades da pessoa encontrada
             encontrado.Nome = pessoa.Nome;
+            encontrado.Email = pessoa.Email;
+            encontrado.Idade = pessoa.Idade;
+            encontrado.DataNascimento = pessoa.DataNascimento;
 
             return Results.Ok(encontrado);
         });
